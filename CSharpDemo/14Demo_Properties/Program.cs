f4b2b87f5bdb81d620ff2d0aa7903f13093ed04d
Priyal Jain <163632872+priyal1350@users.noreply.github.com>
2025-12-26 20:23:40 +0530

    Add files via upload

---
+++ CSharpDemo/14Demo_Properties/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _14Demo_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Eid = 10;
+            int id = emp.Eid;
+            Console.WriteLine($"ID ={id}");
+            emp.Ename = "Hugh Jackman";
+            Console.WriteLine($"Output:{emp.Ename}");
+            emp.EAdress = "New York";
+        }
+    }
+
+    public class Employee
+    {
+        private int _Eid;
+        private string _EName;
+        private string _EAddress;
+
+        public int Eid
+        {
+            set
+            {
+                _Eid = value;
+
+            }
+            get
+            {
+                return _Eid;
+            }
+        }
+        public string Ename
+        {
+            set
+            {
+                if (value != null)
+                {
+                    _EName = "Mr/Mrs." + value;
+                }
+                else
+                {
+                    _EName = "Invalid format";
+                }
+            }
+            get { return _EName; }
+        }
+        public string EAdress
+        {
+            set
+            {
+                _EAddress = value;
+                Console.WriteLine($"Address: {_EAddress}");
+
+            }
+        }
+    }
+}
